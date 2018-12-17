db449e1d3ee09f96f8cf528cefab57487e0e603b
Ruslan <ruslansolodovnyk@gmail.com>
2018-12-17 22:28:31 +0200

    finish step 11

--- app/src/main/java/my/banancheg/com/roomwordsample/MainActivity.kt
+++ app/src/main/java/my/banancheg/com/roomwordsample/MainActivity.kt
@@ -2,11 +2,18 @@ package my.banancheg.com.roomwordsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
